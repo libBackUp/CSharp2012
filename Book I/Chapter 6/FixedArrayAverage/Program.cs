92c897438709e73770c1cdb3e36626132c444f6d
Bill Sempf <bill@pointweb.net>
2012-08-27 21:41:51 -0400

    Added code from chapters

---
+++ Book I/Chapter 6/FixedArrayAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿// FixedArrayAverage -- Average a fixed array of numbers using a loop.
+namespace FixedArrayAverage
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] doublesArray = { 5, 2, 7, 3.5, 6.5, 8, 1, 9, 1, 3 };
+            // Accumulate the values in the array into the variable sum.
+            double sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum = sum + doublesArray[i];
+            }
+            // Now calculate the average.
+            double average = sum / 10;
+            Console.WriteLine(average);
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
---
+++ Book I/Chpater 4/OverloadedOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOperator
+{
+    public class AddOne
+    {
+        public int x;
+
+        public static AddOne operator +(AddOne a, AddOne b)
+        {
+            AddOne addone = new AddOne();
+            addone.x = a.x + b.x + 1;
+            return addone;
+        }
+    }
+
+public class Program{
+        static void Main(string[] args)
+        {
+            AddOne foo = new AddOne();
+            foo.x = 2;
+            AddOne bar = new AddOne();
+            bar.x = 3;
+            //And 2 + 3 now is 6...
+            Console.WriteLine((foo + bar).x.ToString());
+            Console.Read();
+        }
+
+    }
+}
