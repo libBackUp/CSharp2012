e1961c16ec8960b893a67c4ab7eb2cdfc3f01024
Bill Sempf <bill@pointweb.net>
2012-09-05 22:47:58 -0400

    Started Book 2

---
+++ Book II/Chapter 4/InvokeMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿// InvokeMethod -- Invoke a member method through the object.
+using System;
+namespace InvokeMethod
+{
+    class Student
+    {
+        // The name information to describe a student
+        public string firstName;
+        public string lastName;
+        // SetName -- Save name information. (Nonstatic.)
+        public void SetName(string fName, string lName)
+        {
+            firstName = fName;
+            lastName = lName;
+        }
+        // ToNameString -- Convert the student object into a
+        //    string for display. (Nonstatic.)
+        public string ToNameString()
+        {
+            string s = firstName + " " + lastName;
+            return s;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Student student = new Student();
+            student.SetName("Stephen", "Davis"); // Call instance method.
+            Console.WriteLine("Student's name is "
+                             + student.ToNameString());
+            // Wait for user to acknowledge.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
